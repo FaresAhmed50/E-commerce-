e6c5212b9d72bdd5a10107b3defe91d77e44d52b
fares <fares.ahmed15967@aiet.edu.eg>
2025-02-15 04:01:48 +0200

    creating the basic Floder stracture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {FlowbiteService} from './Shared/Services/flowbite.service';
+import {RouterLink} from '@angular/router';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
   title = 'E-commerce';
+
+  _flowbiteService = inject(FlowbiteService);
+  _breakpointObserver = inject(BreakpointObserver);
+   screenSize!: boolean;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this._flowbiteService.loadFlowbite( flowbite => {});
+    this._breakpointObserver.observe([
+      "(max-width: 768px)"
+    ]).subscribe((result: BreakpointState) => {
+      if (result.matches) {
+        console.log(result.matches);
+        this.screenSize = result.matches;
+      } else {
+        console.log(false)
+        this.screenSize = result.matches;
+      }
+    });
+  }
+
+
 }
