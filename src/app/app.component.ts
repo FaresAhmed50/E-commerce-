a405552a682a96821bb98487801063b0b186a48c
fares <fares.ahmed15967@aiet.edu.eg>
2025-02-17 01:27:59 +0200

    Creating the Footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import {NavbarComponent} from './Shared/Layout/navbar/navbar.component';
 import {RouterOutlet} from '@angular/router';
+import {FooterComponent} from './Shared/Layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     NavbarComponent,
-    RouterOutlet
+    RouterOutlet,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
