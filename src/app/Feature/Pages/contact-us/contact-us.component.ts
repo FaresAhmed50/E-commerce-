92c261581cddcb00ece75a8e087a582762df2580
fares <fares.ahmed15967@aiet.edu.eg>
2025-02-27 13:57:36 +0200

    Creating the style of the card component

--- src/app/Feature/Pages/contact-us/contact-us.component.ts
+++ src/app/Feature/Pages/contact-us/contact-us.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {Card} from '../../../Shared/Interface/card/card';
+import {CardComponent} from '../../../Shared/UI/card/card.component';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
 export class ContactUsComponent {
+
+  card : Card[] = [
+    {
+      icon : "mail.png",
+      title : "Hello",
+      paragraph : "Test"
+    }
+  ];
 
 }
--- src/app/Shared/UI/card/card.component.ts
+++ src/app/Shared/UI/card/card.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
+import {Card} from '../../Interface/card/card'
 
 @Component({
   selector: 'app-card',
   imports: [],
   templateUrl: './card.component.html',
+  standalone: true,
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+
+  @Input() card!: Card[];
 
 }
