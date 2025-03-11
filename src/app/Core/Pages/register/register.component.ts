2f91c1aa44d323a98672b4e12cce540b074c9dc7
fares <fares.ahmed15967@aiet.edu.eg>
2025-03-11 22:41:46 +0200

    Finishing the tamplate for the register component

--- src/app/Core/Pages/register/register.component.ts
+++ src/app/Core/Pages/register/register.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+    imports: [
+        RouterLink
+    ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+
+  passwordeye : boolean = false;
+
+
+  seePasword(){
+    this.passwordeye = !this.passwordeye;
+    console.log(this.passwordeye);
+  }
 
 }
