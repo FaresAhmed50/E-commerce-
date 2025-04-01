8853ccb466a7b0e53300caa34e0541df23705971
fares <fares.ahmed15967@aiet.edu.eg>
2025-04-01 05:15:59 +0200

    Fixing the inject of the flowbit service

--- src/app/Shared/Services/Flowbit/flowbite.service.ts
+++ src/app/Shared/Services/Flowbit/flowbite.service.ts
@@ -1,4 +1,4 @@
-import {Inject, Injectable, PLATFORM_ID} from '@angular/core';
+import {inject, Inject, Injectable, InjectionToken, PLATFORM_ID} from '@angular/core';
 import {isPlatformBrowser} from '@angular/common';
 
 @Injectable({
@@ -7,11 +7,10 @@ import {isPlatformBrowser} from '@angular/common';
 
 export class FlowbiteService {
 
-  @Inject(PLATFORM_ID) private platformId: any
-
+  private _platformId :  any = inject(PLATFORM_ID);
 
   loadFlowbite(callback: (flowbite: any) => void) {
-    if (isPlatformBrowser(this.platformId)) {
+    if (isPlatformBrowser(this._platformId)) {
       import('flowbite').then(flowbite => {
         callback(flowbite);
       });
