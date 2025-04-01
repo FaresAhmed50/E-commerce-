38a82f8a77414d6caf5e61996a5e5e12d3c3a200
fares <fares.ahmed15967@aiet.edu.eg>
2025-04-01 05:19:32 +0200

    back to the old inject

--- src/app/Shared/Services/Flowbit/flowbite.service.ts
+++ src/app/Shared/Services/Flowbit/flowbite.service.ts
@@ -1,4 +1,4 @@
-import {inject, Inject, Injectable, InjectionToken, PLATFORM_ID} from '@angular/core';
+import {Inject, Injectable, PLATFORM_ID} from '@angular/core';
 import {isPlatformBrowser} from '@angular/common';
 
 @Injectable({
@@ -7,10 +7,10 @@ import {isPlatformBrowser} from '@angular/common';
 
 export class FlowbiteService {
 
-  private _platformId :  any = inject(PLATFORM_ID);
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
 
   loadFlowbite(callback: (flowbite: any) => void) {
-    if (isPlatformBrowser(this._platformId)) {
+    if (isPlatformBrowser(this.platformId)) {
       import('flowbite').then(flowbite => {
         callback(flowbite);
       });
