4012122afeca403ccb2ee963bfce7212e93562e5
fares <fares.ahmed15967@aiet.edu.eg>
2025-04-03 05:27:57 +0200

    Creating the product service and the categories service and the carditem component

---
+++ src/app/Shared/UI/cardItem/card-item/card-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-item',
+  imports: [],
+  templateUrl: './card-item.component.html',
+  standalone: true,
+  styleUrl: './card-item.component.scss'
+})
+export class CardItemComponent {
+
+}
