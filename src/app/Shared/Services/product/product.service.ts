1d93cd5d2610d2c341b976d038262b2218bbb933
fares <fares.ahmed15967@aiet.edu.eg>
2025-04-05 19:51:02 +0200

    adding the filtered product service

--- src/app/Shared/Services/product/product.service.ts
+++ src/app/Shared/Services/product/product.service.ts
@@ -17,7 +17,16 @@ export class ProductService {
   }
 
   getSpecificProduct(productId: string) : Observable<any> {
-    return this._httpClient.get(`${this.env}products/${productId}`);
+    return this._httpClient.get(`${this.env.BaseURL}products/${productId}`);
+  }
+
+  getFilterdProducts(spacificproduct : string) : Observable<Product[]> {
+
+    return this.getAllProducts().pipe(
+      map((products ) => products.data.filter(
+        (product : Product) => product?.category?.name.toLowerCase().includes(spacificproduct.toLowerCase())
+      ))
+    )
   }
 
 
